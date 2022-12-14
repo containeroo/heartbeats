5ef8d6d920a4ccb5afb45295779a2c5a09f1dab2
gi8 <giottolino@gmail.com>
2022-12-14 08:38:33 +0100

    search also uuid

--- internal/handlers/status.go
+++ internal/handlers/status.go
@@ -21,16 +21,28 @@ func Status(w http.ResponseWriter, req *http.Request) {
 Status: {{ if .Status }}{{ .Status }}{{ else }}-{{ end }}
 LastPing: {{ .TimeAgo .LastPing }}`
 
+	// if no heartbeat is given, return all heartbeats
 	if heartbeatName == "" {
 		textTmpl := fmt.Sprintf("%s%s\n%s", "{{ range . }}", txtFormat, "{{end}}")
 		WriteOutput(w, http.StatusOK, outputFormat, &internal.HeartbeatsServer.Heartbeats, textTmpl)
 		return
 	}
 
-	heartbeat, err := internal.HeartbeatsServer.GetHeartbeatByName(heartbeatName)
-	if err != nil {
-		WriteOutput(w, http.StatusNotFound, outputFormat, ResponseStatus{Status: "nok", Error: err.Error()}, "Status: {{ .Status }}\nError: {{  .Error }}")
-		return
+	var heartbeat *internal.Heartbeat
+	var err error
+
+	if IsValidUUID(heartbeatName) {
+		heartbeat, err = internal.HeartbeatsServer.GetHeartbeatByUUID(heartbeatName)
+		if err != nil {
+			WriteOutput(w, http.StatusNotFound, outputFormat, &ResponseStatus{Status: "nok", Error: err.Error()}, "Status: {{ .Status }}\nError: {{  .Error }}")
+			return
+		}
+	} else {
+		heartbeat, err = internal.HeartbeatsServer.GetHeartbeatByName(heartbeatName)
+		if err != nil {
+			WriteOutput(w, http.StatusNotFound, outputFormat, &ResponseStatus{Status: "nok", Error: err.Error()}, "Status: {{ .Status }}\nError: {{  .Error }}")
+			return
+		}
 	}
 
 	heartbeatStates := &HeartbeatStatus{
